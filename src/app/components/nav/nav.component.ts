c1556547ace4c81d5fabca100ef24e79c6223375
Edilberto Rios Garcia <dredysteam@gmail.com>
2023-05-11 08:59:01 -0400

    header & nav c created

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  list = [
+    {
+      route: 'dashboard',
+      name: 'Home',
+    },
+    {
+      route: 'howitworks',
+      name: 'How It Works',
+    },
+    {
+      route: 'quotation',
+      name: 'Quotation',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
