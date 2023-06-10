8307275fb470f2b603077d00192393b2a46322b0
Edilberto Rios Garcia <dredysteam@gmail.com>
2023-06-10 09:43:00 -0400

    firebase configuration & crud quotation s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AngularFireModule } from '@angular/fire/compat';
+
 // feature modules
 import { AuthModule } from 'src/auth/auth.module';
 import { MainModule } from 'src/main/main.module';
@@ -12,6 +14,12 @@ import { AppComponent } from './app.component';
 //components
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
+
+// firebase variables auto created
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 const ROUTES: Routes = [
   {
@@ -28,6 +36,10 @@ const ROUTES: Routes = [
     RouterModule.forRoot(ROUTES),
     AuthModule,
     MainModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent],
