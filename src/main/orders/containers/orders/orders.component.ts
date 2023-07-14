fdb3f594dc37acecccef06a3932bd5a42e4ac96d
Edilberto Rios Garcia <dredysteam@gmail.com>
2023-07-14 00:59:54 -0400

    orders component changes

--- src/main/orders/containers/orders/orders.component.ts
+++ src/main/orders/containers/orders/orders.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../../orders.service';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
+  orders: any;
 
-  constructor() { }
+  constructor(private ordersService: OrdersService) {}
 
   ngOnInit(): void {
+    this.ordersService.getQuotations().subscribe((res: any) => {
+      this.orders = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.id = e.payload.id;
+        console.log(data);
+        return data;
+      });
+    });
   }
-
 }
